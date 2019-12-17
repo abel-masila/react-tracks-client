0afbad6e7480d28d40d0c202a4f7904febbab8de
Abel <abelmasila@gmail.com>
2019-12-17 17:17:38 +0300

    Added audio player

--- src/components/Shared/AudioPlayer.js
+++ src/components/Shared/AudioPlayer.js
@@ -1,5 +1,10 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
-const AudioPlayer = () => <div>AudioPlayer</div>;
+const AudioPlayer = ({ url }) => (
+  <div>
+    <ReactPlayer height="30px" width="500px" controls={true} url={url} />
+  </div>
+);
 
 export default AudioPlayer;
--- src/components/Track/TrackList.js
+++ src/components/Track/TrackList.js
@@ -38,7 +38,7 @@ const TrackList = ({ classes, tracks }) => (
                 </Link>
               }
             />
-            <AudioPlayer />
+            <AudioPlayer url={track.url} />
           </ListItem>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className={classes.details}>
